a5e733864cdb93a49bedb7a1d74345cc2ba27ab2
andy Sun <andysun@andygzsun-mb2.tencent.com>
2023-06-08 22:51:28 +0800

    feat: ui美化

--- Webcliper/src/popup.js
+++ Webcliper/src/popup.js
@@ -1,37 +1,21 @@
 // 通过ID找到按钮
-const button = document.getElementById("changeColor");
 const myText = document.getElementById("contextText");
 const summaryBtn = document.getElementById("summary");
 
 window.onload = function () {
-  chrome.runtime.sendMessage({ action: "popup" })
+  document.querySelector('.spinner-border').style.display = 'none';
 }
+
+summaryBtn.addEventListener("click", (e) => {
+  chrome.runtime.sendMessage({ action: "popup" })
+  document.querySelector('.spinner-border').style.display = 'inline-block';
+  summaryBtn.disabled = true;
+})
 
 chrome.runtime.onMessage.addListener(async (msg, sender) => {
   console.log(msg, sender)
+  // 请求完成后移除菊花加载动画实例
+  document.querySelector('.spinner-border').style.display = 'none';
+  summaryBtn.disabled = false;
   myText.value = msg.content
-  // if (msg.action == "set_content") {
-  //   // 调用Chrome接口取出当前标签页
-  //   const httpC = new XMLHttpRequest();
-  //   const baseUrl = "http://127.0.0.1:8080/openai/summary";
-
-  //   httpC.open("POST", baseUrl, true);
-  //   httpC.setRequestHeader("Content-type", "application/json");
-
-  //   const req = { content: msg.content }
-
-  //   httpC.onload = function () {
-  //     if (httpC.status != 200) { // HTTP error?
-  //       // 处理 error
-  //       alert('Error: ' + httpC.status);
-  //       return;
-  //     }
-
-  //     // 获取来自 xhr.response 的响应
-  //     console.log(httpC.response)
-  //     const obj = JSON.parse(httpC.response)
-  //     myText.value = httpC.response
-  //   };
-  //   httpC.send(JSON.stringify(req));
-  // }
 });
